34154c1ffe7ab619aaf7c158aa0a9410eed10061
Joe Kramer <joe.kramer.cu@gmail.com>
2016-01-07 21:00:20 -0700

    Adding some basic project files

---
+++ binding.gyp
@@ -0,0 +1,9 @@
+{
+  "targets": [
+    {
+      "target_name": "rawcan",
+      "sources": [ "src/module.cpp" ],
+      "cflags": [ "-std=c++14" ]
+    }
+  ]
+}
---
+++ src/module.cpp
@@ -0,0 +1,18 @@
+#include <node.h>
+
+using v8::Local;
+using v8::Object;
+
+namespace rawcan
+{
+class Channel
+{
+};
+
+void init(Local<Object> exports)
+{
+    TextBuffer::init(exports);
+}
+
+NODE_MODULE(rawcan, init)
+}
