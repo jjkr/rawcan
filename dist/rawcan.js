41c4b2daa60ca105288dcd50a3ead53c6a80bce0
Joe Kramer <joe.kramer.cu@gmail.com>
2016-01-09 22:57:59 -0700

    Simple test passing

--- dist/rawcan.js
+++ dist/rawcan.js
@@ -3,11 +3,14 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Socket = undefined;
 
 var _events = require('events');
 
 var _events2 = _interopRequireDefault(_events);
+
+var _bindings = require('bindings');
+
+var _bindings2 = _interopRequireDefault(_bindings);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
@@ -17,13 +20,13 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var CANWrap = require('bindings')('can_wrap');
+var CANWrap = (0, _bindings2.default)('can_wrap').CANWrap;
 
 var BIND_STATE_UNBOUND = 0;
 var BIND_STATE_BINDING = 1;
 var BIND_STATE_BOUND = 2;
 
-var Socket = exports.Socket = function (_EventEmitter) {
+var Socket = function (_EventEmitter) {
   _inherits(Socket, _EventEmitter);
 
   function Socket() {
@@ -39,3 +42,7 @@ var Socket = exports.Socket = function (_EventEmitter) {
 
   return Socket;
 }(_events2.default);
+
+exports.default = {
+  Socket: Socket
+};
