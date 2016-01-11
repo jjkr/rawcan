8414ad8fd450d2191bc8fb8cfbf8e6e323313d50
Joe Kramer <joe.kramer.cu@gmail.com>
2016-01-10 23:16:20 -0700

    Trying to get node 0.10 working

--- dist/rawcan.js
+++ dist/rawcan.js
@@ -22,7 +22,7 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var CANWrap = (0, _bindings2.default)('can_wrap').CANWrap;
+var CANWrap = (0, _bindings2.default)('can_wrap.node').CANWrap;
 
 var Socket = function (_EventEmitter) {
   _inherits(Socket, _EventEmitter);
