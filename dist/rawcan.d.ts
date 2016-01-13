7dabe5af2b1bb2adb50cbf5a32b2d90eeadd8c49
Joe Kramer <joe.kramer.cu@gmail.com>
2016-01-13 11:02:46 -0700

    Convert rawcan.js to typescript

--- dist/rawcan.d.ts
+++ dist/rawcan.d.ts
@@ -2,6 +2,22 @@
 import { EventEmitter } from 'events';
 export declare class Socket extends EventEmitter {
     private _handle;
-    constructor(iface: string);
+    private _bound;
+    private _sendQueue;
+    constructor(iface?: string);
+    bind(iface: string): Socket;
+    send(id: number, buffer: string | Buffer, callback?: (err: number) => void): void;
+    setFilter(filter: number, mask: number): void;
+    close(): void;
+    ref(): void;
+    unref(): void;
     _onSent(err: number): void;
+    _healthCheck(): void;
 }
+export declare function createSocket(iface: string): Socket;
+export declare const EFF_FLAG: number;
+export declare const RTR_FLAG: number;
+export declare const ERR_FLAG: number;
+export declare const SFF_MASK: number;
+export declare const EFF_MASK: number;
+export declare const ERR_MASK: number;
--- lib/rawcan.ts
+++ lib/rawcan.ts
@@ -3,24 +3,107 @@ import {EventEmitter} from 'events';
 /// <reference path="./can_wrap.d.ts"/>
 import {CANWrap} from './can_wrap';
 
+interface SendRequest {
+  id : number;
+  buffer : Buffer;
+  callback : (err: number) => void;
+}
+
 export class Socket extends EventEmitter {
   private _handle : CANWrap;
+  private _bound : boolean;
+  private _sendQueue : SendRequest[];
 
-  constructor(iface: string) {
+  constructor(iface?: string) {
     super();
     this._handle = new CANWrap();
     this._handle.onSent((err) => { this._onSent(err); });
+    this._handle.onMessage(
+        (id, buffer) => { this.emit('message', id, buffer); });
+    this._handle.onError((err) => { this.emit('error', err); });
+    this._bound = false;
+    if (iface) {
+      this.bind(iface);
+    }
+  }
+
+  bind(iface: string) : Socket {
+    if (this._bound) {
+      throw new Error('Socket is already bound');
+    }
+
+    this._healthCheck();
+
+    const err = this._handle.bind(iface);
+    if(err != 0) {
+      throw new Error('Failed to bind: ' + err);
+    }
+
+    this._bound = true;
+    return this;
+  }
+
+  send(id: number, buffer: string|Buffer, callback?: (err: number) => void) {
+    if (typeof buffer === 'string') {
+      buffer = new Buffer(buffer.toString());
+    }
+
+    this._healthCheck();
+    const sending = this._sendQueue.length > 0;
+    this._sendQueue.push({id: id, buffer: <Buffer>buffer, callback: callback});
+
+    if (!sending) {
+      this._handle.send(id, <Buffer>buffer);
+    }
+  }
+
+  setFilter(filter: number, mask: number) {
+    this._healthCheck();
+    this._handle.setFilter(filter, mask);
+  }
+
+  close() {
+    this._healthCheck();
+    this._handle.close();
+    this._handle = undefined;
+    this.emit('close');
+  }
+
+  ref() {
+    this._healthCheck();
+    this._handle.ref();
+  }
+
+  unref() {
+    this._healthCheck();
+    this._handle.unref();
   }
 
   _onSent(err: number) {
-    //const sent = this._sendQueue[0];
-    //if (sent.callback) {
-    //  sent.callback(err);
-    //}
-    //this._sendQueue.shift();
-    //const next = this._sendQueue[0];
-    //if (this._sendQueue.length > 0) {
-    //  this._handle.send(next.id, next.buffer);
-    //}
+    const sent = this._sendQueue[0];
+    if (sent.callback) {
+      sent.callback(err);
+    }
+    this._sendQueue.shift();
+    const next = this._sendQueue[0];
+    if (next) {
+      this._handle.send(next.id, next.buffer);
+    }
+  }
+
+  _healthCheck() {
+    if (!this._handle) {
+      throw new Error('Not running');
+    }
   }
 }
+
+export function createSocket(iface: string) { return new Socket(iface); }
+
+export const EFF_FLAG : number = 0x80000000; // extended frame format
+export const RTR_FLAG : number = 0x40000000; // remote transmission request
+export const ERR_FLAG : number = 0x20000000; // error
+
+export const SFF_MASK : number = 0x7FF; // standard frame format - 11 bits
+export const EFF_MASK : number = 0x1FFFFFFF; // extended frame format - 29 bits
+export const ERR_MASK : number = 0x1FFFFFFF;
